96cc216ae7f9e4f272324f058ffabd5c58dea5d6
hoplitfr <contact@hoplit.fr>
2024-12-08 22:53:35 +0100

    Disabling voting buttons if user already voted
    Users can only vote once for each quote. Votes are stored in sessions,
    which will prevent users from voting multiple times.

--- class-quote-vote-voting.php
+++ class-quote-vote-voting.php
@@ -32,6 +32,20 @@ class Quote_Vote_Voting
     }
 
     /**
+     * Check if the user has already voted for a quote.
+     *
+     * @param int $post_id The ID of the quote.
+     * @return bool True if the user has already voted, false otherwise.
+     */
+    public static function has_voted($post_id)
+    {
+        if (!session_id()) {
+            session_start();
+        }
+        return isset($_SESSION['quote_vote_' . $post_id]);
+    }
+
+    /**
      * Handle the vote submission via AJAX.
      *
      * @return void
@@ -41,6 +55,12 @@ class Quote_Vote_Voting
         if (isset($_POST['post_id']) && isset($_POST['vote_type'])) {
             $post_id = intval($_POST['post_id']);
             $vote_type = sanitize_text_field($_POST['vote_type']);
+
+            // Check if the user has already voted
+            if (self::has_voted($post_id)) {
+                wp_send_json_error(array( 'message' => 'You have already voted for this quote.' ));
+                return;
+            }
 
             $current_votes = get_post_meta($post_id, 'quote_votes', true);
             $current_votes = $current_votes ? $current_votes : 0;
@@ -53,11 +73,18 @@ class Quote_Vote_Voting
 
             update_post_meta($post_id, 'quote_votes', $current_votes);
 
+            // Set a session variable to indicate that the user has voted
+            if (!session_id()) {
+                session_start();
+            }
+            $_SESSION['quote_vote_' . $post_id] = 'voted';
+
             wp_send_json_success(array( 'votes' => $current_votes ));
         }
 
         wp_send_json_error();
     }
+
 
     /**
      * Add vote buttons to the content of quote posts.
@@ -72,10 +99,12 @@ class Quote_Vote_Voting
             $votes = get_post_meta($post_id, 'quote_votes', true);
             $votes = $votes ? $votes : 0;
 
+            $has_voted = self::has_voted($post_id);
+
             $vote_buttons = '
             <div class="quote-vote">
-                <button class="vote-button" data-post-id="' . $post_id . '" data-vote-type="upvote">Upvote</button>
-                <button class="vote-button" data-post-id="' . $post_id . '" data-vote-type="downvote">Downvote</button>
+                <button class="vote-button" data-post-id="' . $post_id . '" data-vote-type="upvote" ' . ($has_voted ? 'disabled' : '') . '>Upvote</button>
+                <button class="vote-button" data-post-id="' . $post_id . '" data-vote-type="downvote" ' . ($has_voted ? 'disabled' : '') . '>Downvote</button>
                 <span id="vote-count-' . $post_id . '">' . $votes . '</span>
             </div>';
 
@@ -83,4 +112,5 @@ class Quote_Vote_Voting
         }
         return $content;
     }
+
 }
--- quote-vote.js
+++ quote-vote.js
@@ -1,7 +1,10 @@
 jQuery(document).ready(function($) {
-    $('.vote-button').on('click', function() {
-        var post_id = $(this).data('post-id');
-        var vote_type = $(this).data('vote-type');
+    $('.vote-button').on('click', function(e) {
+        e.preventDefault();
+
+        var $button = $(this);
+        var post_id = $button.data('post-id');
+        var vote_type = $button.data('vote-type');
 
         $.ajax({
             url: quoteVoteAjax.ajax_url,
@@ -14,7 +17,13 @@ jQuery(document).ready(function($) {
             success: function(response) {
                 if (response.success) {
                     $('#vote-count-' + post_id).text(response.data.votes);
+                    $button.prop('disabled', true);
+                } else {
+                    alert(response.data.message);
                 }
+            },
+            error: function() {
+                alert('An error occurred while processing your vote.');
             }
         });
     });
